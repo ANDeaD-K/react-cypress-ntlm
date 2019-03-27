c8d9dc7f915d5d3b7b3763f4b39300fca45a0fcd
Andrey Kislov <andrej.kislov@spellabs.ru>
2019-03-27 15:42:39 +0300

    Create project

--- cypress/integration/examples/login.js
+++
@@ -1,6 +0,0 @@
-describe('Login example', () => {
-  it('should login to my pag', () => {
-    cy.visit('/')
-    cy.url().should('include', '/login')
-  })
-})
---
+++ cypress/integration/first_test.spec.js
@@ -0,0 +1,9 @@
+describe('First Test', function() {
+    it('Visit site', function() {
+        // Uncomment this line for use NTML authentication and change SP_SITE_URL, LOGIN and PASSWORD
+        // cy.ntlm("SP_SITE_URL", "LOGIN", "PASSWORD");
+
+        // And then you can call cy.visit
+        // cy.visit("http://localhost/site/test");
+    });
+});
